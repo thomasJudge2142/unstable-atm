15565be38f660588a9f3a839656c43a456e5fca3
thomasJudge2142 <thomas.pjudge1@gmail.com>
2026-02-27 21:55:15 +0000

    2 bug down??

--- tests/tests.cc
+++ tests/tests.cc
@@ -72,5 +72,13 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   transactions[{12345678, 1234}].push_back(
       "Deposit - Amount: $32000.00, Updated Balance: $72099.90");
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
+  REQUIRE(atm.CheckBalance(12345678, 1234) == 72099.90);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
+}
+
+TEST_CASE("My Tests", "[My Test 1]") {
+  Atm atm;
+  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
+  REQUIRE_THROWS(atm.RegisterAccount(12345678, 1000, "John", 500.00));
+  REQUIRE_NOTHROW(atm.RegisterAccount(35243453, 1234, "able", 500.00));
 }
